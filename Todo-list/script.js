bc80c15ff6ea7d64a0626b69a21b702b07102817
parineeta-cpu <cpari4097@gmail.com>
2025-06-28 21:57:36 +0530

    Add files via upload

--- Todo-list/script.js
+++ Todo-list/script.js
@@ -5,23 +5,30 @@ function added(){
     var a = document.querySelector('#display');
     var para = document.createElement('p');
     var but = document.createElement('button');
+    var para2 = document.createElement('p');
     b=a.value;
-    para.innerText=b;
+    para2.innerText=b;
     a.value="";
     main.appendChild(para);
+    para.appendChild(para2);
     but.innerText="❌";
     function remove(){
         para.remove();
     }
+
     function cut(){
-        para.style.textDecoration="line-through";
-        para.style.opacity="0.8";
+        para2.style.textDecoration="line-through";
+        para2.style.opacity="0.8";
     }
     but.addEventListener("click",remove);
      para.appendChild(but); 
      but.classList.add('cancelbut');  
-     para.addEventListener("click",cut); 
-     
+     para2.addEventListener("click",cut); 
+     if(b === ""){
+        para.innerText="Please enter something..";
+        console.log(para);
+        para.style.color="red";
+     }
 }
 
 btn.addEventListener("click",added);
