2c8536238b6c9381ec1b9f6e925f11c6c3eab4be
parineeta-cpu <cpari4097@gmail.com>
2025-07-09 11:40:28 +0530

    Add files via upload

--- projects/form-validation/script.js
+++ projects/form-validation/script.js
@@ -1,7 +1,6 @@
 const submitBtn=document.querySelector("#submitbtn");
-
 function form(){
-  event.preventDefault();
+ 
    const nameInput= document.querySelector("#name");
    const emailInput=document.querySelector("#email");
    const passwordInput=document.querySelector("#password");
@@ -16,31 +15,38 @@ function form(){
    if(!emailInput.checkValidity()){
     alert("Please enter a valid email");
      return;
+      
    }
   
    if(passwordInput.value.length<6){
     alert("Password must be atleast 6 characters long.")
      return;
+      
    }
    if(passwordInput.value!==cnfrmPasswordInput.value){
 alert("Passwords do not match");
  return;
+  
    }
 
    if(numberInput.value.length!==10){
     alert("Please enter a valid 10-digit number.")
      return;
+    
    }
    if(genderInput.value===""){
     alert("Please select your gender");
      return;
+      
    }
    if(!checkbox.checked){
     alert("Please agree to all Terms&conditions");
      return;
+    
    }
    else {
     alert("Form submitted successfully✔");
    }
 }
+
 submitBtn.addEventListener("click", form);
