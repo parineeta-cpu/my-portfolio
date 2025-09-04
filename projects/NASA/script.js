ab2a19d5b7c39ccde22c435994500e24b100253b
parineeta-cpu <cpari4097@gmail.com>
2025-09-04 16:46:53 +0530

    Add files via upload

--- projects/NASA/script.js
+++ projects/NASA/script.js
@@ -1,25 +1,25 @@
 const main = document.querySelector('#main');
 const info = document.querySelectorAll('.info');
 const generateBtn = document.querySelector('#generateBtn');
-const image=document.querySelector("#image");
-const para=document.querySelector(".request");
+const image = document.querySelector("#image");
+if(!dateSelector){
+    dateSelector=new Date().toISOString().split("T")[0];
+}
 function generate(){
-const dateSelector = document.querySelector('#dateSelector').value;
+    let dateSelector = document.querySelector('#dateSelector').value;
 const apiKey="b8OWmaTHWutzcBntD71u84F1IEIb1zdq5VChCQbK";
 const link = fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${dateSelector}`);
-
 link.then((res)=>{
 return res.json();
 }).then((data)=>{
-info[0].innerHTML = `<h2> Title:${data.title}</h2>`
+info[0].innerHTML = `<h2>Title:${data.title}</h2>`
 info[1].innerText = `Date:${data.date}`
 info[2].innerText = `Description:${data.explanation}`
 image.src=data.url;
-para.style.display="none";
 }).catch((error)=>{
 alert("Something Went Wrong");
 })
 console.log(dateSelector);
-
 }
+generate();
 generateBtn.addEventListener("click",generate);
